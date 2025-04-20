ebfd15cbac3d7a7f3e61ece5509a158265e3b865
Adnyx <theo.dub@yahoo.com>
2025-04-20 17:01:08 +0200

    Added all the website's files

---
+++ script/main.js
@@ -0,0 +1,11 @@
+const themeButton = document.getElementById('theme-toggle')
+
+themeButton.addEventListener('click', function() {
+    if (document.body.classList.contains('light-theme')) {
+        document.body.classList.remove('light-theme');
+        document.body.classList.add('dark-theme');
+      } else {
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
+      }
+})
