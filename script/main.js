552b6ca0f92a992ef51c7783e2e08232b11547bf
Adnyx <theo.dub@yahoo.com>
2025-04-21 19:23:43 +0200

    CHANGE: change the big picture feature to one with a lightbox plugin + lots of meta data

--- script/main.js
+++ script/main.js
@@ -1,25 +1,13 @@
-const themeButton = document.getElementById('theme-toggle')
+window.addEventListener('DOMContentLoaded', () => {
+  const themeButton = document.getElementById('theme-toggle');
 
-const clickableImg = document.getElementById('smallImage')
-const bigImage = document.getElementById('bigImage')
-const darkBackground = document.getElementById('darkBackground')
-
-themeButton.addEventListener('click', function() {
-    if (document.body.classList.contains('light-theme')) {
-        document.body.classList.remove('light-theme');
-        document.body.classList.add('dark-theme');
+  themeButton.addEventListener('click', function() {
+      if (document.body.classList.contains('light-theme')) {
+          document.body.classList.remove('light-theme');
+          document.body.classList.add('dark-theme');
       } else {
-        document.body.classList.remove('dark-theme');
-        document.body.classList.add('light-theme');
+          document.body.classList.remove('dark-theme');
+          document.body.classList.add('light-theme');
       }
-})
-
-clickableImg.addEventListener("click", function() {
-  bigImage.style.display = "block";
-  darkBackground.style.display = "block";
-});
-
-darkBackground.addEventListener("click", function() {
-  bigImage.style.display = "none";
-  darkBackground.style.display = "none";
+  });
 });
