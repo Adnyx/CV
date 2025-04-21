b1db118657528eac62b22d667a8b27f301e56c8b
Adnyx <theo.dub@yahoo.com>
2025-04-21 18:51:24 +0200

    FEAT: Added a clickable image that shows in big :3

--- script/main.js
+++ script/main.js
@@ -1,5 +1,9 @@
 const themeButton = document.getElementById('theme-toggle')
 
+const clickableImg = document.getElementById('smallImage')
+const bigImage = document.getElementById('bigImage')
+const darkBackground = document.getElementById('darkBackground')
+
 themeButton.addEventListener('click', function() {
     if (document.body.classList.contains('light-theme')) {
         document.body.classList.remove('light-theme');
@@ -9,3 +13,13 @@ themeButton.addEventListener('click', function() {
         document.body.classList.add('light-theme');
       }
 })
+
+clickableImg.addEventListener("click", function() {
+  bigImage.style.display = "block";
+  darkBackground.style.display = "block";
+});
+
+darkBackground.addEventListener("click", function() {
+  bigImage.style.display = "none";
+  darkBackground.style.display = "none";
+});
